6666ec7eb12ff64a91d165b8aadf17ead9c69e97
Nitsss26 <99978200+Nitsss26@users.noreply.github.com>
2024-08-11 23:18:52 +0530

    Update axios.js

--- src/store/axios.js
+++ src/store/axios.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 
 const instance = axios.create({
-    baseURL: "https://dreamscloudtechbackend.vercel.app/api"
+    baseURL: "https://dreamscloudtechbackend.onrender.com/api"
 })
 
 
